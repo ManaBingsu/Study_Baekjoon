57fb6cd67776133ceded1c4c1149cd4bcd034085
ManaBingsu <duoq9901@gmail.com>
2020-12-08 10:39:05 +0900

    feat : 1009_분산처리.cpp

---
+++ ETC/1009_분산처리.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int T, a, b;
+
+int main() {
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> a >> b;
+	}
+	return 0;
+}
